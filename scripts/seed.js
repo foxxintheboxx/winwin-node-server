50c49fd0bd3afb4ebfff5e282d608303c89cedd6
Ian Fox <ianfox41@gmail.com>
2017-03-28 16:09:26 +0200

    Create crumbs with Path options

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,5 +1,6 @@
 const deepstream = require('deepstream.io-client-js');
 const _ = require("underscore");
+const LatLon = require("../latlon-spherical");
 const constants = deepstream.CONSTANTS
 
 const client = deepstream(process.env.HOST).login({ username: "nodeserver", password:"1234" });
@@ -7,11 +8,24 @@ client.on( 'connectionStateChanged', ( connectionState ) => {
   if (connectionState === constants.CONNECTION_STATE.OPEN) {
     let count;
     let completed = 0;
+    let needed= 45 + 15 * 1 * 2;
     let lat0 = 41385985;
-    let lat1 = 41391024;
+    let lat1 = 41401024;
     let lng0 = 2164719;
     let lng1 = 2196712;
-    const div = Math.pow(10, 6);
+    let directions = 1;
+    const callback = (err) => {
+      if (err) {
+        console.log('Record set with error:', err)
+      } else {
+        console.log('Record set without error')
+      }
+      completed += 1;
+      if (completed === needed) {
+        client.close();
+      }
+    };
+    const pow = Math.pow(10, 6);
     _.each( process.argv, (arg) => {
       const argArray = arg.split("=");
       const value = parseFloat(argArray[1])
@@ -19,39 +33,101 @@ client.on( 'connectionStateChanged', ( connectionState ) => {
       if (argArray[0] === "lat1") lat1 = value
       if (argArray[0] === "lng0") lng0 = value
       if (argArray[0] === "lng1") lng1 = value
+      if (argArray[0] === "dir") directions = value
     });
     for ( count = 0; count < 15; count++ ) {
-      const lat = _.random(lat0, lat1)
-      const lng = _.random(lng0, lng1)
-      for ( let i = 0; i < 3; i++) {
-        const _lat = _.random(lat - 30, lat + 30)
-        const _lng = _.random(lng - 30, lng + 30)
-        const recordName = "object/" + client.getUid();
-        let record = client.record.getRecord(recordName);
-        console.log("CREATING " + recordName);
-        record.whenReady(() => {
-          record.set({
-            location: {
-              type: "Point",
-              coordinates: { lng: _lng / div, lat: _lat / div }
-            },
-            value: _.random(1, 5) * 10,
-            owner: "",
-            type: "coin"
-          },err => {
-            if (err) {
-              console.log('Record set with error:', err)
-            } else {
-              console.log('Record set without error')
-            }
-            completed += 1;
-            if (completed === 45) {
-              client.close();
-            }
-          });
+
+      // variables for this set of coins
+      const point = new LatLon(_.random(lat0, lat1) / pow, _.random(lng0, lng1) / pow);
+      const crumbs = _.random(2, 3);
+      if (crumbs === 3) needed + 1;
+      const opts = { color: getRandomColor() };
+      const pathOpts = { uid: client.getUid() };
+      const avgDistance = _.random(600, 650); // meters
+
+      // variables for this set of crumbs
+      for (let d = 0; d < directions; d++) {
+        const crumb0 = client.record.getRecord(objRecordName());
+        const crumb1 = client.record.getRecord(objRecordName());
+        const crumb2 = (crumbs > 3) ? client.record.getRecord(objRecordName()) : null;
+
+        const bearingOffset = _.random(0, 100); // make  points for all crumbs
+        const crumb0Point = point.destinationPoint(avgDistance, bearingOffset * (1 + d));
+        const crumb1Point = point.intermediatePointTo(crumb0Point, _.random(40, 65) / 100);
+        const crumb2Point = point.intermediatePointTo(crumb0Point, _.random(60, 75) / 100);
+
+        let crumbObjs = [
+          { r: crumb0, coor: crumb0Point},
+          { r: crumb1, coor: crumb1Point},
+          { r: crumb2, coor: crumb2Point}
+        ];
+        crumbObjs = _.filter(crumbObjs, c => { return c.r });
+
+        crumbObjs.sort(( a, b ) => {
+            const distA = point.distanceTo(a.coor);
+            const distB = point.distanceTo(b.coor);
+            if (distA === distB) return 0;
+            if (distA > distB) return -1;
+            return 1;
         });
+
+        _.each(crumbObjs, (c, i) => {
+          console.log("CREATING CRUMB");
+          const crumbPathOpts = _.extend({ count: i }, pathOpts);
+          const crumbOpts = _.extend({ pathOpts: crumbPathOpts }, opts);
+          const type = "crumb";
+          makeObject(c.coor.lat, c.coor.lon, c.r, callback, type, crumbOpts);
+        });
+
+      }
+
+      // make Coins
+      for ( let i = 0; i < 3; i++) {
+        const _lat = _.random(point.lat * pow - 30, point.lat * pow + 30) / pow;
+        const _lng = _.random(point.lon * pow - 30, point.lon * pow + 30) / pow;
+        const record = client.record.getRecord(objRecordName());
+        const value = _.random(1, 5) * 10;
+        const coinPathOpts = _.extend({ count: crumbs }, pathOpts);
+        const coinOpts = _.extend({ value, pathOpts: coinPathOpts }, opts);
+        const type = "coin";
+        makeObject(_lat, _lng, record, callback, type, coinOpts);
+        console.log("CREATING COIN" + count);
       }
     }
     console.log("COMPLETE")
   }
 });
+
+const objRecordName = () => "object/" + client.getUid();
+
+const makeObject = (lat, lng, record, callback, type, opts) => {
+   if (!opts.pathOpts) opts.pathOpts = { uid: "", count: 0 };
+   if (!opts.contents) opts.contents = "";
+   if (!opts.color) opts.color = "";
+   if (!opts.value) opts.value = 0;
+   record.whenReady(r => {
+     r.set({
+       location: {
+         type: "Point",
+         coordinates: { lng, lat }
+       },
+       type: type,
+       value: opts.value,
+       color: opts.color,
+       owner: "",
+       path_uid: opts.pathOpts.uid,
+       path_count: opts.pathOpts.count,
+     },err => callback(err)
+     );
+   });
+
+}
+
+const getRandomColor = () => {
+   const letters = '0123456789ABCDEF';
+   let color = '#';
+   for (let i = 0; i < 6; i++ ) {
+      color += letters[Math.floor(Math.random() * 16)];
+   }
+   return color;
+}
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 const DSClient = require('./ds-server.js');
 
-DSClient.login({ username: "nodeserver", password: "1234" }, (sucess, data) => { DSClient.setup(data.uid) });
+DSClient.login({ username: "nodeserver", password: "1234" }, (sucess, data) => {
+    DSClient.setup(data.uid)
+});
