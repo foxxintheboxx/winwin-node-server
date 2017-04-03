e927858bd72a96df48330f8a03333291f03a4bb1
Ian Fox <ianfox41@gmail.com>
2017-04-03 11:19:23 -0700

    Make optional number of crumbs for only coins

--- scripts/seed.js
+++ scripts/seed.js
@@ -19,7 +19,9 @@ client.on( "connectionStateChanged", ( connectionState ) => {
     let lng1 = 2196712;
     let directions = 3;
     let coinsPerCluster = 1;
-    let needed= clusters * coinsPerCluster + clusters * directions;
+    let minCrumbs = 0;
+    let maxCrumbs = 0;
+    let needed= clusters * coinsPerCluster + clusters * directions * minCrumbs;
     const callback = (err) => {
       if (err) {
         console.log("Record set with error:", err)
@@ -45,48 +47,49 @@ client.on( "connectionStateChanged", ( connectionState ) => {
 
       // variables for this set of coins
       const point = new LatLon(_.random(lat0, lat1) / pow, _.random(lng0, lng1) / pow);
-      const crumbs = _.random(2, 3);
+      const crumbs = _.random(minCrumbs, maxCrumbs);
       if (crumbs === 3) needed + 1;
       const opts = { color: getRandomColor() };
       const pathOpts = { uid: client.getUid() };
       const avgDistance = _.random(600, 650); // meters
 
       // variables for this set of crumbs
-      for (let d = 0; d < directions; d++) {
-        const crumb0 = client.record.getRecord(objRecordName());
-        const crumb1 = client.record.getRecord(objRecordName());
-        const crumb2 = (crumbs > 2) ? client.record.getRecord(objRecordName()) : null;
+      if (crumbs > 0) {
+        for (let d = 0; d < directions; d++) {
+          const crumb0 = client.record.getRecord(objRecordName());
+          const crumb1 = client.record.getRecord(objRecordName());
+          const crumb2 = (crumbs > 2) ? client.record.getRecord(objRecordName()) : null;
 
-        const bearingOffset = _.random(0, 100); // make  points for all crumbs
-        const crumb0Point = point.destinationPoint(avgDistance, bearingOffset * (1 + d));
-        const crumb1Point = point.intermediatePointTo(crumb0Point, _.random(40, 65) / 100);
-        const crumb2Point = point.intermediatePointTo(crumb0Point, _.random(60, 75) / 100);
+          const bearingOffset = _.random(0, 100); // make  points for all crumbs
+          const crumb0Point = point.destinationPoint(avgDistance, bearingOffset * (1 + d));
+          const crumb1Point = point.intermediatePointTo(crumb0Point, _.random(40, 65) / 100);
+          const crumb2Point = point.intermediatePointTo(crumb0Point, _.random(60, 75) / 100);
 
-        let crumbObjs = [
-          { r: crumb0, coor: crumb0Point},
-          { r: crumb1, coor: crumb1Point},
-          { r: crumb2, coor: crumb2Point}
-        ];
-        crumbObjs = _.filter(crumbObjs, c => { return c.r });
+          let crumbObjs = [
+            { r: crumb0, coor: crumb0Point},
+            { r: crumb1, coor: crumb1Point},
+            { r: crumb2, coor: crumb2Point}
+          ];
+          crumbObjs = _.filter(crumbObjs, c => { return c.r });
 
-        crumbObjs.sort(( a, b ) => {
-            const distA = point.distanceTo(a.coor);
-            const distB = point.distanceTo(b.coor);
-            if (distA === distB) return 0;
-            if (distA > distB) return -1;
-            return 1;
-        });
+          crumbObjs.sort(( a, b ) => {
+              const distA = point.distanceTo(a.coor);
+              const distB = point.distanceTo(b.coor);
+              if (distA === distB) return 0;
+              if (distA > distB) return -1;
+              return 1;
+          });
 
-        _.each(crumbObjs, (c, i) => {
-          console.log("CREATING CRUMB");
-          const crumbPathOpts = _.extend({ count: i }, pathOpts);
-          const crumbOpts = _.extend({ pathOpts: crumbPathOpts }, opts);
-          const type = "crumb";
-          makeObject(c.coor.lat, c.coor.lon, c.r, callback, type, crumbOpts);
-        });
+          _.each(crumbObjs, (c, i) => {
+            console.log("CREATING CRUMB");
+            const crumbPathOpts = _.extend({ count: i }, pathOpts);
+            const crumbOpts = _.extend({ pathOpts: crumbPathOpts }, opts);
+            const type = "crumb";
+            makeObject(c.coor.lat, c.coor.lon, c.r, callback, type, crumbOpts);
+          });
 
+        }
       }
-
       // make Coins
       for ( let i = 0; i < coinsPerCluster; i++) {
         const _lat = _.random(point.lat * pow - 30, point.lat * pow + 30) / pow;
