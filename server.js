ffae6160d63d577938f9359f3857ce63706d84e2
Ian Fox <ianfox41@gmail.com>
2017-04-07 15:25:14 -0700

    create http server for heroku bind error

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const DSClient = require('./ds-server.js');
+const express = require('express');
+const http = require('http');
 
-DSClient.login({ username: "nodeserver", password: "1234" }, (sucess, data) => {
+const server = http.createServer(express());
+server.listen(process.env.PORT || 6023, () => {
+  DSClient.login({ username: "nodeserver", password: "1234" }, (sucess, data) => {
     console.log(sucess)
     console.log(data)
     DSClient.setup(data.uid)
+  });
 });
+
